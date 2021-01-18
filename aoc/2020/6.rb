536a66b5d78d54c3106ecaee9a503c6d19fbb05f
cer-0 <cer0@riseup.net>
2021-01-18 16:34:04 -0600

    Fix variable typo, add comments

--- aoc/2020/6.rb
+++ aoc/2020/6.rb
@@ -1,21 +1,26 @@
 #!/usr/bin/env ruby
+#
+# My previous idea was pretty complicated, I tried to add letters to an array and see if
+# there were zero duplicates, good for the first part, but not useful for the second.
+# George suggested using a hash, this made things so much easier.
 
 sum_count = 0
 people_counter = 0
-questions_encontered = Hash.new(0)
+questions_encountered = Hash.new(0)
 questions_everyone_answered = 0
 
 File.foreach("6.input", chomp: true) { |line|
   if not line.empty?
-    line.each_char { |char| questions_encontered[char] += 1 }
+    line.each_char { |char| questions_encountered[char] += 1 }
     people_counter += 1
   else
-    questions_encontered.each_pair { |key, val|
+    questions_encountered.each_pair { |key, val|
       questions_everyone_answered += 1 if val == people_counter
     }
-    sum_count += questions_encontered.keys.length
-    questions_encontered = Hash.new(0)
+    sum_count += questions_encountered.keys.length
+    questions_encountered = Hash.new(0)
     people_counter = 0
   end
 }
+
 p sum_count, questions_everyone_answered
