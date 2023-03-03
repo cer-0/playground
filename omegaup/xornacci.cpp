3d545df1f2c94f93600d3aff64400587b2e9f3ee
cer-0 <cer0@riseup.net>
2023-03-03 13:48:18 -0600

    Add xornacci from omegaup

---
+++ omegaup/xornacci.cpp
@@ -0,0 +1,23 @@
+// https://omegaup.com/arena/Paketaxo#problems/Xornacci-cpp
+#include <iostream>
+
+int xoracci(int a, int b, int n){
+    if (n % 3 == 0)
+        return a;
+    else if (n % 3 == 1)
+        return b;
+    else if (n % 3 == 2)
+        return a ^ b;
+    else
+        return a;
+}
+
+int main() {
+    int t, a, b, n;
+    std::cin >> t;
+    for(int i = 0; i < t; i++) {
+        std::cin >> a >> b >> n;
+        std::cout << xoracci(a, b, n) << "\n";
+    }
+    return 0;
+}
